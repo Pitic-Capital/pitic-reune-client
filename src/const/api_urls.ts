a34063f9246257c10ac997784a6638845b7189df
Cesar <admingit@lecosy.com.mx>
2025-08-05 11:34:20 -0700

    prod fix

--- src/const/api_urls.ts
+++ src/const/api_urls.ts
@@ -1,2 +1,2 @@
 export const API_URL = "https://api-reune.condusef.gob.mx";
-export const API_URL = "https://api-reune-pruebas.condusef.gob.mx";
+export const API_URL_TEST = "https://api-reune-pruebas.condusef.gob.mx";
